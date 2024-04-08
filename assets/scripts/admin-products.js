9906b72112828cb40d72dc32513c2cba9cc6fd66
Jesus Lopez <jlmwsk32@gmail.com>
2024-04-07 19:48:23 -0600

    add: writeDestination funcionality

--- assets/scripts/admin-products.js
+++ assets/scripts/admin-products.js
@@ -81,5 +81,17 @@ const showError= (errorMessage)=>{
 }
 
 const writeDestination = (destiny)=>{
-  
+    const {title, date, price, description}=destiny;
+    const titulo=document.createElement("p");
+    titulo.innerText=title;
+    document.querySelector("contenedorParrafos").appendChild(titulo);
+    const fecha=document.createElement("p");
+    fecha.innerText=date;
+    document.querySelector("contenedorParrafos").appendChild(fecha);
+    const precio=document.createElement("p");
+    precio.innerText=price;
+    document.querySelector("contenedorParrafos").appendChild(precio);
+    const descripcion=document.createElement("p");
+    descripcion.innerText=description;
+    document.querySelector("contenedorParrafos").appendChild(descripcion);
 }
