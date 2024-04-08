8451bf6d85dfebeab2e062c79fd80e2ac3f3c30f
Ulimax <uliortegao@gmail.com>
2024-04-07 19:21:46 -0600

    ci: adding message of succes to user and adding some styles

--- assets/scripts/admin-products.js
+++ assets/scripts/admin-products.js
@@ -1,12 +1,52 @@
 const register= document.forms["register-form"];
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger"
+    },
+    buttonsStyling: false
+  });
 register.addEventListener("submit", (e)=>{
     e.preventDefault();
-const newDestination = {
-    title: register.elements["destination"].value,
-    date: register.elements["date"].value,
-    price: register.elements["price"].value,
-    description: register.elements["description"].value,
-}    
+
+    swalWithBootstrapButtons.fire({
+        title: "¿Estás seguro?",
+        text: "¡No podrás revertir esta acción!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, Guardar!",
+        cancelButtonText: "No, Cancelar!",
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+          const newDestination = {
+            title: register.elements["destination"].value,
+            date: register.elements["date"].value,
+            price: register.elements["price"].value,
+            description: register.elements["description"].value,
+        }    
+        if( isValid( newDestination) ){
+          writeDestination( newDestination); 
+        }  
+          swalWithBootstrapButtons.fire({
+            title: "¡Destino Guardado!",
+            text: "Tu destino ha sido guardado",
+            icon: "success"
+          });
+        } else if (
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          register.reset()
+          swalWithBootstrapButtons.fire({
+            title: "Cancelado",
+            text: "",
+            icon: "error"
+          });
+        }
+      });
+
+   
 });
 const isValid = (newDestination)=>{
     let valid = true;
@@ -24,5 +64,22 @@ const isValid = (newDestination)=>{
         message.push("La longitud mínima de la descripción es de 100 caracteres");
     }
 
+    if( ! valid ) showError( message );
+    else showError( [] );
 
+    return valid;
+
+}
+
+const showError= (errorMessage)=>{
+  let alert = "";
+  for (let message of errorMessage ){
+      alert += `<li>${message}</li>`
+  }
+
+  document.getElementById("error-message").innerHTML= alert;
+}
+
+const writeDestination = (destiny)=>{
+  
 }
