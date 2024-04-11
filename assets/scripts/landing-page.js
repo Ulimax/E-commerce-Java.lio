b51a4023c31247ce2b655d36f21035ccc6921959
Miguel <Miguel_a.GM@hotmail.com>
2024-04-11 07:07:13 -0600

    feat: agregada animacion de carga

--- assets/scripts/landing-page.js
+++ assets/scripts/landing-page.js
@@ -1,5 +1,99 @@
+const renderContent = () => {
+    document.querySelector("main").innerHTML = `
+    <section>
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+             <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            </div> 
+        
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="" >
+                <img src="./assets/img/img-carrusel/globos-GTO.jpg" class="d-block w-100 imagencarrusel" alt="Globos aerostáticos en Guanajuato">
+                <div class="carousel-caption">
+                  <h3> <strong>Globos Aerostáticos en Guanajuato</strong> </h3>
+                  <p>Del 16 al 20 de noviembre 2024 </p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="5000">
+                <img src="./assets/img/img-carrusel/maruata.jpg" class="d-block w-100 imagencarrusel" alt="Atardecer en Maruata">
+                <div class="carousel-caption">
+                  <h3> <strong>Maruata</strong> </h3>
+                  <p>Del 3 al 5 de mayo 2024 </p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="5000">
+                <img src="./assets/img/img-carrusel/nevado-de-colima.jpg" class="d-block w-100 imagencarrusel" alt="Paisaje del Nevado de Colima">
+                <div class="carousel-caption">
+                  <h3> <strong>Nevado de Colima</strong> </h3>
+                  <p>Del 18 al 29 de mayo 2024</p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="5000">
+                <img src="./assets/img/img-carrusel/Nevado-De-Toluca-National-Park.jpg" class="d-block w-100 imagencarrusel" alt="Atardecer en el Nevado de Toluca">
+                <div class="carousel-caption">
+                  <h3> <strong> Nevado de Toluca </strong> </h3>
+                  <p>Del 16 al 20 de noviembre 2024 </p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="5000">
+                <img src="./assets/img/img-carrusel/tepoztlan-paracaidismo.JPG" class="d-block w-100 imagencarrusel" alt="Paracaidismo en Tepoztlan">
+                <div class="carousel-caption">
+                  <h3> <strong> Paracaidismo y Tepoztlán 2° fecha </strong> </h3>
+                  <p>Del 25 al 26 de mayo 2024 </p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+      </section>
+    
+        <div id="whatsapp-chat">
+          <a href="https://wa.me/3221749411" target="_blank">
+            <img src="./assets/img/img-carrusel/icono-whatsApp.svg" alt="Icono de WhatsApp">
+          </a>
+        </div>
+    
+      <section class="products-container">
 
- const cart = [];
+        <div class="header">
+            <p class="headertext"> Próximos Viajes</p>
+             
+          </div>
+         
+         <div class="container"> 
+             <div id="root"></div>
+             <div class="sidebar">
+             <div class="head"> <p>Mochila</p></div>
+             <div id="cartItem">Tu mochila está vacía </div>
+             <div class="foot">
+                 <h3>Total</h3>
+                 <h2 id="total">$0.00</h2>
+         
+             </div>
+           </div>
+         </div>
+    
+      </section>
+    `;
+}
+
+setTimeout(() => {
+    renderContent();
+}, 0);
+
+
+const cart = [];
 
  async function getData(url) {
      try {
@@ -22,7 +116,7 @@
                  <p>${product.title}</p>
                  <p>${product.date}</p>
                  <h2>$ ${product.price}.00</h2>
-                 <button onclick='addtocart(${JSON.stringify(product)})'>Añadir al carrito</button>
+                 <button onclick='addtocart(${JSON.stringify(product)})'>Añadir a la mochila</button>
              </div>
          </div>
      `).join('');
@@ -54,7 +148,7 @@
      
      const total = cart.reduce((acc, item) => acc + item.price, 0);
      
-     document.getElementById('cartItem').innerHTML = cart.length === 0 ? "Your cart is empty" : cartItemsHtml;
+     document.getElementById('cartItem').innerHTML = cart.length === 0 ? "Tu mochila está vacía" : cartItemsHtml;
      document.getElementById("total").innerHTML = `$ ${total}.00`;
  }
  
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 const getPageName = () => {
-     return window.location.pathname.split("/").pop() ? window.location.pathname.split("/").pop() : "index.html";
-}
+  return window.location.pathname.split("/").pop() ? window.location.pathname.split("/").pop() : "index.html";
+};
 
 /* const renderMisc = () => {
      document.querySelector("head").innerHTML += `
@@ -10,9 +10,9 @@ const getPageName = () => {
 } */
 
 const renderNavbar = () => {
-     const pages = ["index.html", "contact-form.html", "blog.html", "about-us.html"];
+  const pages = ["index.html", "contact-form.html", "about-us.html"];
 
-     document.querySelector("header").innerHTML = `
+  document.querySelector("header").innerHTML = `
      <nav class="navbar navbar-expand bg-body-transparent d-none d-md-block">
      <div class="container-fluid">
           <a class="navbar-brand ms-2" href="/index.html">
@@ -22,20 +22,14 @@ const renderNavbar = () => {
                <img src="/assets/icons/logo-letters.png" alt="Logo-letras" width="200" height="auto">
           </a>
 
-          <form id="xpd-nav-search-grp" class="d-flex titillium-web-semibold ms-auto me-4" role="search">
-              <input id="xpd-nav-search" class="form-control me-1" type="search" placeholder="Búsqueda" aria-label="Search">
-              <button id="xpd-nav-search-btn" class="btn titillium-web-semibold ms-1" type="submit">Buscar</button>
-          </form>
+          
 
-          <ul class="navbar-nav navbar-text mb-2 mb-lg-0 d-flex align-items-center">
+          <ul class="navbar-nav navbar-text mb-2 ms-auto mb-lg-0 d-flex align-items-center">
                <li class="nav-item xpd-nav-item">
                     <a class="nav-link mx-2 titillium-web-semibold" aria-current="page" href="/index.html">Inicio</a>
                </li>
                <li class="nav-item xpd-nav-item">
                     <a class="nav-link mx-2 titillium-web-semibold" aria-current="page" href="/assets/pages/contact-form.html">Contáctanos</a>
-               </li>
-               <li class="nav-item xpd-nav-item">
-                    <a class="nav-link mx-2 titillium-web-semibold" aria-current="page" href="/assets/pages/blog.html">Blog</a>
                </li>
                <li class="nav-item xpd-nav-item">
                     <a class="nav-link ms-2 me-3 titillium-web-semibold" aria-current="page" href="/assets/pages/about-us.html">Acerca de nosotros</a>
@@ -81,9 +75,6 @@ const renderNavbar = () => {
                               <li class="nav-item cpd-nav-item">
                                    <a class="nav-link active titillium-web-semibold" aria-current="page" href="/assets/pages/contact-form.html">Contáctanos</a>
                               </li>
-                              <li class="nav-item cpd-nav-item">
-                                   <a class="nav-link active titillium-web-semibold" aria-current="page" href="/assets/pages/blog.html">Blog</a>
-                              </li>
                          <li class="nav-item cpd-nav-item">
                               <a class="nav-link active titillium-web-semibold" aria-current="page" href="/assets/pages/about-us.html">Acerca de nosotros</a>
                          </li>
@@ -99,62 +90,96 @@ const renderNavbar = () => {
                          </li>
                          <hr class="border bg-dark-subtle border-2 opacity-50">
                     </ul>
-                    <form class="d-flex titillium-web-semibold ms-auto me-3" role="search">
-                         <input id="cpd-nav-search" class="form-control me-1" type="search" placeholder="Búsqueda" aria-label="Search">
-                         <button id="cpd-nav-search-btn" class="btn titillium-web-semibold ms-1" type="submit">Buscar</button>
-                    </form>
+                    
                </div>
           </div>
           </div>
      </nav>`;
-     
-     document.querySelectorAll("li.xpd-nav-item > a")[pages.indexOf(getPageName())].classList.add("disabled");
-     document.querySelectorAll("li.cpd-nav-item > a")[pages.indexOf(getPageName())].classList.add("disabled");
+
+  document.querySelectorAll("li.xpd-nav-item > a")[pages.indexOf(getPageName())].classList.add("disabled");
+  document.querySelectorAll("li.cpd-nav-item > a")[pages.indexOf(getPageName())].classList.add("disabled");
 };
 
 const renderFooter = () => {
-     document.querySelector("footer").innerHTML = `
-     <div class="container-fluid footer">
+  document.querySelector("footer").innerHTML = `
+    <div class="footer-decorator"></div>
+     <div class="container-fluid footer d-flex flex-column titillium-web-semibold">
+          <div class="mt-4">
           <div class="row">
-               <div class="ftr-section col-12 col-sm-4 my-4 p-4 text-light titillium-web-semibold">
-                    <div id="ftr-logo-cntr" class="d-flex my-4">
-                         <img class="m-auto" src="/assets/icons/logo.png" width="100px" alt="Logo nuevo mundo travel">
-                    </div>
-                    <p class="titillium-web-semibold my-4">Av. Adolfo López Mateos Sur 2077, Jardines Plaza del Sol, 44510 Guadalajara, Jalisco, México.</p>
-                    <p class="titillium-web-semibold">¡Conócenos!</p>
-                    <div class="ftr-smedia-cntr d-flex">
-                         <a href=""><img class="ftr-smedia-i"src="/assets/img/Instagram_logo_2016.svg.png" alt="Instagram"></a>
-                         <a href=""><img class="ftr-smedia-i"src="/assets/img/Facebook_f_logo_(2019).svg" alt="Facebook"></a>
-                         <a href=""><img class="ftr-smedia-i"src="/assets/img/WhatsApp.svg.png" alt="Whatsapp"></a>
-                    </div>
+            <div class="ftr-section d-flex flex-column col-12 col-md-4 p-4 text-light">
+            <div class="d-flex my-4">
+                <img src="/assets/icons/logo-white.png" class="m-auto" alt="Logo nuevo mundo travel" width="100">
+              </div>
+              <span class="mt-4">Av. Adolfo López Mateos Sur 2077, Jardines Plaza del Sol, 44510 Guadalajara, Jalisco, México.</span>
+              <span class="mt-4">¡Conócenos!</span>
+              <div class="ftr-smedia-cntr d-flex mt-4">
+                <a href="https://www.instagram.com/nuevomundo_travel"><img class="ftr-smedia-i" src="/assets/icons/instagram-white.svg" alt="Instagram"></a>
+                <a href="https://www.facebook.com/nuevomundotraveltuours"><img class="ftr-smedia-i" src="/assets/icons/facebook-white.svg" alt="Facebook"></a>
+                <a href="https://wa.me/3221749411"><img class="ftr-smedia-i" src="/assets/icons/whatsapp-white.svg" alt="WhatsApp"></a>
+              </div>
+            </div>
+            <div class="ftr-section col-12 col-md-4 mt-4 p-4 text-light">
+               <span class="titillium-web-semibold fw-bold">Navegación</span>
+              <ul class="ftr-nav titillium-web-semibold mt-4">
+              <li><a href="/index.html">Inicio</a>
+                <li><a href="/assets/pages/contact-form.html">Contacto</a>
+                <li><a href="/assets/pages/about-us.html">Acerca de nosotros</a>
+              </ul>
+               <div class="d-flex flex-column mt-4">
+               <span class="titillium-web-semibold fw-bold atention-label">Atención</span>
+               <span class="titillium-web-semibold mt-4">nuevomundotravell@gmail.com</span>
+               <span class="titillium-web-semibold">Lunes a domingo de 8 a.m. a 8 p.m.</span>
                </div>
-               <div class="ftr-section col-12 col-sm-4 my-4 p-4 text-light titillium-web-semibold">
-                    <p class="titillium-web-semibold fw-bold">Navegación</p>
-                    <ul class="ftr-nav titillium-web-semibold my-4">
-                         <li><a href="/assets/pages/contact-form.html">Contacto</a>
-                         <li><a href="/assets/pages/blog.html">Blog</a>
-                         <li><a href="/assets/pages/about-us.html">Acerca de nosotros</a>
-                    </ul>
-               </div>
-               <div class="ftr-section col-12 col-sm-4 my-4 p-4 text-light titillium-web-semibold rounded-3">
-                    <a class="titillium-web-semibold" href="">Politica de privacidad</a>
-                    <a class="titillium-web-semibold" href="">Aviso legal</a>
-                    <a class="titillium-web-semibold" href="">Cookies</a>
-                    <p class="titillium-web-semibold mt-4">Impulsado por</p>
-                    <a href="https://mexico.generation.org/">
-                    <img src="/assets/icons/generation-logo-white.png" width="120px" class="mb-4" height="auto" alt="Logo Generation"></a>
+            </div>
+            <div class="ftr-section d-flex flex-column col-12 col-md-4 my-4 p-4 text-light rounded-3">
+              <a href="/assets/pages/privacy-policy.html">Política de privacidad</a>
+              <a href="/assets/pages/privacy-policy.html">Aviso legal</a>
+              <a href="/assets/pages/privacy-policy.html">Cookies</a>
+              <span class="mt-4">Impulsado por</span>
+              <a class="mt-4" href="https://mexico.generation.org/">
+              <img src="/assets/icons/generation-logo-white.svg" width="120px" class="mb-4" height="auto" alt="Logo Generation"></a>
+
+            </div>
                </div>
           </div>
-          <div class="row text-light text-center p-0 mt-auto titillium-web-semibold">
-               <p class="mt-auto">Copyright &copy 2024 Nuevo Mundo Travel</p>
-               <p class="">Todos los derechos reservados</p>
+          <div class="mt-auto">
+          <div class="row text-light text-center p-0 ftr-copyright">
+            <span class="mt-auto">Copyright \u00a9 2024 Nuevo Mundo Travel</span>
+            <span class="">Todos los derechos reservados</span>
           </div>
-     </div>
-     `;
-}
+          </div>
+        </div>
+  `;
+};
+
+const includeLinks = () => {
+  document.head.innerHTML += `
+    <link rel="icon" href="/assets/icons/logo-planet.png">
+    <link rel="stylesheet" href="/assets/styles/default.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+  `;
+};
+
+const renderFirstLoading = () => {
+  document.body.innerHTML += `
+  <div id="loading-label">
+    <figure id="loading">
+      <img id="loading-static" src="/assets/icons/loading-label.png" alt="">
+      <img id="loading-animated" src="/assets/icons/loading.gif" alt="">
+    </figure>
+  </div>
+  `;
+  setTimeout(() => document.querySelector("#loading-label").classList.add("d-none"), 2000);
+};
 
 const mainRender = (function () {
-     renderNavbar();
-     document.getElementsByClassName("nav-login-i")[Math.round(Math.random())].style.display = "inline-block";
-     renderFooter();
+  if (!sessionStorage.getItem("session"))
+    renderFirstLoading();
+  sessionStorage.setItem("session", "active");
+  includeLinks();
+  renderNavbar();
+  document.getElementsByClassName("nav-login-i")[Math.round(Math.random())].style.display = "inline-block";
+  renderFooter();
 }());
