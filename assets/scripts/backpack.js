88d7b08a80e4578793d3ec1f7dd0d7f614dc0aae
Miguel <Miguel_a.GM@hotmail.com>
2024-05-01 20:56:45 -0600

    ci: reestructuración variada

--- assets/scripts/backpack.js
+++ assets/scripts/backpack.js
@@ -1,30 +1,30 @@
-
 const storedCart = localStorage.getItem("backpack");
 const cart = storedCart ? JSON.parse(storedCart) : null;
-console.log(cart[0].price)
-function displaycart() {
+
+function displayCart() {
     const cartItemsHtml = cart.map((item, index) => `
         <div class='cart-item'>
             <div class='row-img'>
                 <img class='rowimg' src=${item.image}>
             </div>
             <p style='font-size:12px;'>${item.title}</p>
-            <h2 style='font-size: 12px;'>$ ${item.price}.00</h2>
+            <h2 style='font-size: 12px;'>MXN$ ${item.price}.00</h2>
             <i class='fa-solid fa-trash' onclick='delElement(${index})'></i>
         </div>
     `).join('');
 
     const total = cart.reduce((acc, item) => acc + item.price, 0);
 
-    document.getElementById('cartItem').innerHTML = cart.length === 0 ? "Tu mochila está vacía" : cartItemsHtml;
-    document.getElementById("total").innerHTML = `$ ${total}.00`;
+    document.querySelector(".cart-item").innerHTML = cart.length === 0 ? "Tu mochila está vacía" : cartItemsHtml;
+    document.getElementById("total").innerHTML = `<span>MXN$ ${total}.00</span><a href="reservation-form.html"><button>Continuar</button></a>`;
+
 }
 
-displaycart()
+displayCart();
 
 function delElement(index) {
     cart.splice(index, 1);   
     const json = JSON.stringify(cart); 
     localStorage.setItem("backpack",json);
-    displaycart()
+    displayCart();
 }
