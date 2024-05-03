fbdd835e5979e92d72fe8937ffeb1b43b548e6ea
Miguel <Miguel_a.GM@hotmail.com>
2024-05-03 07:59:50 -0600

    ci: cambio formato de json home

--- assets/scripts/landing-page.js
+++ assets/scripts/landing-page.js
@@ -30,34 +30,32 @@ function displayProducts(products) {
 function openModal(product) {
 
  const modalContent = `
-        <div class="modal">
-            <div class="modal-content">
-                <span class="close" onclick="closeModal()">&times;</span>
-                <h2>${product.title}</h2>
-               
-                <p> Precio: $${product.price}.00 MXN</p>
-                <ul> ⭐Descripción: ${product.description}
-                <li> 📆Fecha de Salida: ${product.departureDate}</li>
-                <li> 📆Fecha de llegada: ${product.returnDate}</li>
-                <li> Punto de partida: 
-                     <ul>
-                     <li>${product.departurePoint1}</li>
-                     <li>${product.departurePoint2}</li>
-                     <li>${product.departurePoint3}</li>
-                     </ul>
-                </li>
-                
-                <li> Incluye: 
-                    <ul>
-                      <li> ${product.amenity1} </li>
-                      <li> ${product.amenity2} </li>
-                      <li> ${product.amenity3} </li>
-                      <li> ${product.amenity4} </li>
-                      <li> ${product.amenity5} </li>
-                    </ul>
-                </li>
-                <li> Lugares disponibles: ${product.availableSpots}</li>
-                </ul>
+    <div class="modal">
+      <div class="modal-content">
+        <span class="close" onclick="closeModal()">&times;</span>
+        <h2>${product.title}</h2>
+        <p> Precio: $${product.price}.00 MXN</p>
+        <ul> ⭐Descripción: ${product.description}
+          <li> 📆Fecha de Salida: ${product.departureDate}</li>
+          <li> 📆Fecha de llegada: ${product.returnDate}</li>
+          <li> Punto de partida: 
+          <ul>
+            <li>${product.departurePoint1}</li>
+            <li>${product.departurePoint2}</li>
+            <li>${product.departurePoint3}</li>
+          </ul>
+          </li>
+          <li> Incluye: 
+            <ul>
+              <li> ${product.amenity1} </li>
+              <li> ${product.amenity2} </li>
+              <li> ${product.amenity3} </li>
+              <li> ${product.amenity4} </li>
+              <li> ${product.amenity5} </li>
+            </ul>
+          </li>
+          <li> Lugares disponibles: ${product.availableSpots}</li>
+        </ul>
       </div>
     </div>
   `;
@@ -69,9 +67,31 @@ function closeModal() {
 }
 
 function addtocart(product) {
-  const cart = localStorage.getItem("backpack") ? JSON.parse(localStorage.getItem("backpack")) : [];
-  document.querySelector(".xpd-backpack-counter").textContent = cart.push(product);
-  const json = JSON.stringify(cart);
+  const cart = localStorage.getItem("backpack") ? JSON.parse(localStorage.getItem("backpack")).post : [];
+  const reservations = localStorage.getItem("backpack") ? JSON.parse(localStorage.getItem("backpack")).reservations : [];
+  let isNotContained = true;
+  for (let i = 0; i < cart.length; i++) {
+    if (JSON.stringify(cart[i]) === JSON.stringify(product)) {
+      isNotContained = false;
+      reservations[i] = reservations[i] + 1;
+      break;
+    }
+  }
+  if (isNotContained) {
+    reservations.push(1);
+    cart.push(product);
+  }
+  document.querySelector(".xpd-backpack-counter").textContent = reservations.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+  const json = JSON.stringify({
+    "reservations": reservations,
+    "user": {
+      "fullName": "name",
+      "email": "email",
+      "telephone": "telephone"
+    },
+    "post": cart
+  });
   localStorage.setItem("backpack", json);
 
   const toastExample = document.querySelector("#toast-1");
